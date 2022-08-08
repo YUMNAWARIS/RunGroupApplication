6813fd213cd352da381fe763765d1a43a194e254
Yumna Waris <yumnaahwaris@gmail.com>
2022-08-08 07:16:28 +0500

    Models - EntityFrameworkCore Backend

---
+++ RunGroupWebApp/Models/City.cs
@@ -0,0 +1,14 @@
+﻿namespace RunGroupWebApp.Models
+{
+    public class City
+    {
+        public int Id { get; set; }
+        public string CityName { get; set; }
+        public string State { get; set; }
+        public int Zip { get; set; }
+        public string CountryCode { get; set; }
+        public double Latitude { get; set;}
+        public double Longitude { get; set;}
+
+    }
+}
